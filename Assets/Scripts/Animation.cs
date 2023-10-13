724a0f77195925d524a62cf9e609b1fa7cd4fc02
Jaime-student <136556174+Jaime-student@users.noreply.github.com>
2023-10-13 14:45:40 +1100

    Initial commit

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public bool Attack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Attack = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
